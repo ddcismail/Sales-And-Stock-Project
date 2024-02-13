fbae81f1f53fc58e0c56219861e25469c2060876
ddcismail <ismaildedec@gmail.com>
2024-02-13 23:16:19 +0300

    connection connections have been made. ObjectHelper added

---
+++ src/tr/com/is/core/ObjectHelper.java
@@ -0,0 +1,33 @@
+package tr.com.is.core;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import tr.com.is.interfaces.CoreInterface;
+
+public class ObjectHelper extends CoreFields implements CoreInterface {
+
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public Connection getConnection() {
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection(getUrl(), getUserName(), getPassword());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return connection;
+	}
+
+}
