d26a85ab6ab573edfa911a36999a9982b2371a52
ddcismail <ismaildedec@gmail.com>
2024-02-16 22:35:44 +0300

    Data access layer defined

--- src/tr/com/is/interfaces/DalInterface.java
+++ src/tr/com/is/interfaces/DalInterface.java
@@ -1,5 +1,20 @@
 package tr.com.is.interfaces;
 
-public interface DalInterface {
+import java.util.List;
 
+public interface DalInterface<T>
+{
+	//<T> generic interface
+
+	public void Insert (T entitiy);  //veri tabanindan kayit almak icin
+	
+	public List<T> GetAll();  //verilerilistelemk icin
+	
+	public T Delete (T entity);  //contract turunde delete
+	
+	public void Update (T entitiy);
+	
+	public List<T> GetById(int id);
+	
+	
 }
