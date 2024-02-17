c6c6a640529cc7f8a65d1d686a065ca778563897
ddcismail <ismaildedec@gmail.com>
2024-02-17 22:51:32 +0300

    Data access layer package was created for database codes. CategoryDAL class added.

---
+++ src/tr/com/is/dal/CategoryDAL.java
@@ -0,0 +1,41 @@
+package tr.com.is.dal;
+
+import java.util.List;
+
+import tr.com.is.core.ObjectHelper;
+import tr.com.is.interfaces.DalInterface;
+
+public class CategoryDAL<CategoryContract> extends ObjectHelper implements DalInterface<CategoryContract> {
+
+	@Override
+	public void Insert(CategoryContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<CategoryContract> GetAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CategoryContract Delete(CategoryContract entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void Update(CategoryContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<CategoryContract> GetById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
