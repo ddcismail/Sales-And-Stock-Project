fe09b722b3cd7527b7d5c5d830f23dabbf24317f
ddcismail <ismaildedec@gmail.com>
2024-02-28 10:59:14 +0300

    The DAL layer has started to be created. PersonnelDAL was created

---
+++ src/tr/com/is/dal/ProductsDAL.java
@@ -0,0 +1,40 @@
+package tr.com.is.dal;
+
+import java.util.List;
+
+import tr.com.is.core.ObjectHelper;
+import tr.com.is.interfaces. DalInterface;
+
+public class ProductsDAL<ProductsContract> extends ObjectHelper implements DalInterface<ProductsContract>{
+
+	@Override
+	public void Insert(ProductsContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<ProductsContract> GetAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProductsContract Delete(ProductsContract entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void Update(ProductsContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<ProductsContract> GetById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
