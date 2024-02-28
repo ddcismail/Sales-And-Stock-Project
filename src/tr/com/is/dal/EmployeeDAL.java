a7a514d99f252454d96128db9e93dd6151875a0c
ddcismail <ismaildedec@gmail.com>
2024-02-28 12:23:37 +0300

    EmployeeDAL was created

---
+++ src/tr/com/is/dal/EmployeeDAL.java
@@ -0,0 +1,41 @@
+package tr.com.is.dal;
+
+import java.util.List;
+
+import tr.com.is.core.ObjectHelper;
+import tr.com.is.interfaces.DalInterface;
+
+public class EmployeeDAL<EmployeeContract> extends ObjectHelper implements DalInterface<EmployeeContract>{
+
+	@Override
+	public void Insert(EmployeeContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<EmployeeContract> GetAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EmployeeContract Delete(EmployeeContract entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void Update(EmployeeContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<EmployeeContract> GetById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
