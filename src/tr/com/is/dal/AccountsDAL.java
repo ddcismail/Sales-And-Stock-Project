aadc204517bf5f5e61e3846eeaf46d94b296dac9
ddcismail <ismaildedec@gmail.com>
2024-02-29 22:51:39 +0300

    AccountsDAL was created

---
+++ src/tr/com/is/dal/AccountsDAL.java
@@ -0,0 +1,40 @@
+package tr.com.is.dal;
+
+import java.util.List;
+
+import tr.com.is.core.ObjectHelper;
+import tr.com.is.interfaces.DalInterface;
+
+public class AccountsDAL <AccountsContract> extends ObjectHelper implements DalInterface<AccountsContract>{
+
+	@Override
+	public void Insert(AccountsContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<AccountsContract> GetAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AccountsContract Delete(AccountsContract entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void Update(AccountsContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<AccountsContract> GetById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
