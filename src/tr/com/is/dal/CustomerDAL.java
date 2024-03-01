168af6f8feda5f6bf3ca2ce63862ef0ac578d3db
ddcismail <ismaildedec@gmail.com>
2024-03-01 22:25:29 +0300

    CustomerDAL was created

---
+++ src/tr/com/is/dal/CustomerDAL.java
@@ -0,0 +1,40 @@
+package tr.com.is.dal;
+
+import java.util.List;
+
+import tr.com.is.core.ObjectHelper;
+import tr.com.is.interfaces.DalInterface;
+
+public class CustomerDAL <CustomerContract> extends ObjectHelper implements DalInterface<CustomerContract>{
+
+	@Override
+	public void Insert(CustomerContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<CustomerContract> GetAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CustomerContract Delete(CustomerContract entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void Update(CustomerContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<CustomerContract> GetById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
