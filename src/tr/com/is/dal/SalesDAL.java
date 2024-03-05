9055410b1e558b6c48ecec9c3b5f51ee1aae141c
ddcismail <ismaildedec@gmail.com>
2024-03-05 14:13:45 +0300

    SalesDAL was created

---
+++ src/tr/com/is/dal/SalesDAL.java
@@ -0,0 +1,40 @@
+package tr.com.is.dal;
+
+import java.util.List;
+
+import tr.com.is.core.ObjectHelper;
+import tr.com.is.interfaces.DalInterface;
+
+public class SalesDAL <SalesContract> extends ObjectHelper implements DalInterface<SalesContract>{
+
+	@Override
+	public void Insert(SalesContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<SalesContract> GetAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SalesContract Delete(SalesContract entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void Update(SalesContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<SalesContract> GetById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
