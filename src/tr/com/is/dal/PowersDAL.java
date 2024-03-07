d6ad87c506136eb1938f260c29144b3b13a1a087
ddcismail <ismaildedec@gmail.com>
2024-03-07 22:57:34 +0300

    PowersDAL Was created

---
+++ src/tr/com/is/dal/PowersDAL.java
@@ -0,0 +1,40 @@
+package tr.com.is.dal;
+
+import java.util.List;
+
+import tr.com.is.core.ObjectHelper;
+import tr.com.is.interfaces.DalInterface;
+
+public class PowersDAL <PowersContract> extends ObjectHelper implements DalInterface<PowersContract>{
+
+	@Override
+	public void Insert(PowersContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<PowersContract> GetAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PowersContract Delete(PowersContract entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void Update(PowersContract entitiy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<PowersContract> GetById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
